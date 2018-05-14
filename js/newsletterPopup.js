56474b4bcbd82a98eb70aac0cae5001017387d48
Przemek Mensik <przemek.mensik@gmail.com>
2018-05-14 21:21:53 +0200

    notification bar and 3lvl menu done

--- js/newsletterPopup.js
+++ js/newsletterPopup.js
@@ -44,4 +44,35 @@ document.getElementById("close-btn").addEventListener("click", function(){
     newsletter.style.display = "none";
     modal.style.display = "none";
 });
+
+//top notification bar text color
+
+$("#color").change(function () {
+    var color = $(this).val()
+    $('.notification-content').css('color', color);
+}).change();
+
+
+//top notification bar background-color
+$("#background").change(function () {
+    var background = $(this).val()
+    $('.notification').css('background-color', background);
+}).change();
+
+//close notification bar
+$("#notification-close-btn").click(function(){
+    $(".notification").hide();
+    console.log("dziala hide");
+});
+
+//choose between hide and show bar
+$("#show-notification").click(function () {
+    var option = $(this).val()
+      if (option === "show"){
+          $(".notification").show();
+      }else{
+          $(".notification").hide();
+  }
+});
+
 });
