7bc2c7e8ec373252b0f4711f699e784092e227b8
Przemek Mensik <przemek.mensik@gmail.com>
2018-05-15 13:58:11 +0200

    js cleaning

--- js/newsletterPopup.js
+++ js/newsletterPopup.js
@@ -1,49 +1,27 @@
-/*
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
-    console.log("newsletter popup")
 
-    var newsletter = document.getElementById("newsletter") ;
-    console.log(newsletter);
-
-    newsletter.addEventListener("click", function ()  {
-       console.log("newsletter popup");
-    });
-
-    body.addEventListener("onload", function() {
-      alert("Newsletter loaded");
-    });
-
-
-  });
-*/
-/*
-$(window).("load", function() {
-   alert("alert");// code here
-});
-
-
-$(function() {
-alert("alert"); // code here
-});
-*/
 document.addEventListener("DOMContentLoaded", function(event) {
 
-window.addEventListener("load", function(){
+  window.addEventListener("load", function(){
 
-    var newsletter = document.getElementById("newsletter");
+      var newsletter = document.getElementById("newsletter");
+      var modal = document.querySelector(".modal");
+
+
+      setTimeout(function(){ newsletter.style.display = "block"; modal.style.display = "block";}, 1000);
+
+    });
+
+//close modal
+
     var modal = document.querySelector(".modal");
+    console.log(modal);
 
+  document.getElementById("close-btn").addEventListener("click", function(){
 
-    setTimeout(function(){ newsletter.style.display = "block"; modal.style.display = "block";}, 1000);
+      newsletter.style.display = "none";
+      modal.style.display = "none";
 
-});
-  var modal = document.querySelector(".modal");
-  console.log(modal);
-document.getElementById("close-btn").addEventListener("click", function(){
-    newsletter.style.display = "none";
-    modal.style.display = "none";
-});
+    });
 
 //top notification bar text color
 
@@ -54,25 +32,28 @@ $("#color").change(function () {
 
 
 //top notification bar background-color
+
 $("#background").change(function () {
     var background = $(this).val()
     $('.notification').css('background-color', background);
 }).change();
 
 //close notification bar
+
 $("#notification-close-btn").click(function(){
     $(".notification").hide();
     console.log("dziala hide");
 });
 
 //choose between hide and show bar
+
 $("#show-notification").click(function () {
     var option = $(this).val()
       if (option === "show"){
           $(".notification").show();
       }else{
           $(".notification").hide();
-  }
-});
+      }
+  });
 
 });
